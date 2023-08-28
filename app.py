2b8ed719b690458e0b64f5eb13c2bd1e4dff58c9
jrfmathis06 <jrfmathis06@outlook.com>
2023-08-28 08:53:23 -0500

    finished cringyss

--- app.py
+++ app.py
@@ -6,21 +6,21 @@ app = Flask(__name__)
 def home():
     return send_from_directory(app.static_folder + '/home/html', 'index.html')
 
-@app.route('/cringy-ss')
-def cringy_ss():    
-    return send_from_directory(app.static_folder + '/CringySS/html', 'cringyss.html')
-
-@app.route('/bio')
+@app.route('/assignment1/bio')
 def bio():
     return send_from_directory(app.static_folder + '/bio-site/html', 'bio.html')
 
-@app.route('/schedule')
+@app.route('/assignment1/schedule')
 def schedule():
     return send_from_directory(app.static_folder + '/bio-site/html', 'schedule.html')
 
-@app.route('/favorites')
+@app.route('/assignment1/favorites')
 def favorites():
     return send_from_directory(app.static_folder + '/bio-site/html', 'favorites.html')
+
+@app.route('/assignment2/cringy-ss')
+def cringy_ss():    
+    return send_from_directory(app.static_folder + '/CringySS/html', 'cringyss.html')
 
 if __name__ == '__main__':
     print('Running app...')
