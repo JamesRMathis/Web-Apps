d67b233282b4953c0a1fe322be7980d0590b4153
jamesrmathis <jrfmathis06@outlook.com>
2023-09-15 09:01:28 -0500

    finished zen garden

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, send_from_directory, redirect
+from flask import Flask, send_from_directory, redirect, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return send_from_directory(app.static_folder + '/home/html', 'index.html')
+
+@app.route('/hello')
+def hello():
+    name = 'Bob'
+    if request.method == 'GET' and 'name' in request.args:
+        name = request.args['name']
+    return render_template('hello.html', name=name)
 
 @app.route('/assignment1/bio')
 def bio():
@@ -21,6 +28,10 @@ def favorites():
 @app.route('/assignment2/cringy-ss')
 def cringy_ss():    
     return send_from_directory(app.static_folder + '/CringySS/html', 'cringyss.html')
+
+@app.route('/assignment3/zen-garden')
+def zen_garden():    
+    return send_from_directory(app.static_folder + '/ZenGarden/html', 'index.html')
 
 if __name__ == '__main__':
     print('Running app...')
